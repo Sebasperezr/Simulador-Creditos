11b4bf15647246318ad86ddeccc8cd3d58bd43bc
Sebastian Perez <sebasperezr@gmail.com>
2022-11-09 01:58:34 -0500

    Add services

---
+++ js/service/creditService.js
@@ -0,0 +1,14 @@
+
+import { URLAPI } from '../constantes.js';
+const urlCustomer = `${URLAPI}customer/`
+
+
+
+export const get = (idCustomer, id = "") => {
+    const urlCredit =`${urlCustomer}/${idCustomer}/credit/${id}`
+    return fetch(urlCredit)
+        .then((resp) => resp.json())
+        .then((data) => {
+            return data
+        })
+}
---
+++ js/service/customerService.js
@@ -0,0 +1,33 @@
+//Servicio para conectar con la API los datos del Cliente 
+
+
+
+import { URLAPI } from '../constantes.js';
+const urlCustomer = `${URLAPI}customer/`
+
+export const post = (customer) => {
+
+   return fetch(urlCustomer, {
+      method: 'POST',
+      body: JSON.stringify({
+         name: customer.name,
+         cantCredits: customer.cantCredits,
+         dni: customer.dni,
+      }),
+      headers: {
+         'Content-type': 'application/json; charset=UTF-8',
+      },
+   })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+}
+
+
+export const get =  (id = "") => {
+
+    return fetch(`${urlCustomer}${id}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+            return data
+        })
+}
